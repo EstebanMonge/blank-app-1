b1cc1da789a407fdd34cbff807a64c3c9547d97d
Esteban Monge <estebanmonge@riseup.net>
2026-06-10 20:01:58 +0000

    ordenar botones e imagen

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,13 @@
 import streamlit as st
 
 st.title("Votappcion")
-st.image("imagenes/subzero.webp")
 st.write(
     "Bienvenido a Votappcion la aplicación para escoger al mejor"
 )
+st.write(
+    "¿Vota por?"
+)
+st.image("imagenes/subzero.webp")
 st.button("Si")
 st.button("No")
+st.button("Ver resultados")
