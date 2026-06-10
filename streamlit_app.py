8927a6d7bf55e961a1f38bf71d568ca0305d5477
Esteban Monge <estebanmonge@riseup.net>
2026-06-10 19:45:41 +0000

    Prueba

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Votappcion")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Bienvenido a Votappcion la aplicación para escoger al mejor"
 )
